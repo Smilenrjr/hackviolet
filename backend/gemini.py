116fc95565fcde51c044bb42be7950e0ca12b547
Ryan Ray <ryan24@vt.edu>
2026-01-31 22:17:31 -0500

    added gemini api

---
+++ backend/gemini.py
@@ -0,0 +1,12 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+response = client.models.generate_content(
+    model = "gemini-3-flash-preview",
+    contents = "Why is the sky blue"
+)
+print(response.text)
